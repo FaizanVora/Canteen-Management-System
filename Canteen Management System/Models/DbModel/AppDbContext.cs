c2240877e856e882417825af12f16cdca2498a6b
FaizanVora <92642594+FaizanVora@users.noreply.github.com>
2022-11-02 22:13:57 +0530

    error solved and menu is working

--- Canteen Management System/Models/DbModel/AppDbContext.cs
+++ Canteen Management System/Models/DbModel/AppDbContext.cs
@@ -3,7 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Canteen_Management_System.Models.DbModel
 {
+
     public class AppDbContext : IdentityDbContext
+
+    /*public class AppDbContext : DbContext*/
+
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
